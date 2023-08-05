68bc661b606e79723671bd84cad10edb8bd2842e
gshemanth <hemanthshankar010@gmail.com>
2023-08-05 08:00:32 +0530

    my first commit

---
+++ src/Practice/Demo.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		String hungry="yess";
+		if(hungry=="yess")
+		{
+			System.out.println("eat");
+		}
+		
+		else
+		{
+			System.out.println("read");
+		}
+	}
+
+}
